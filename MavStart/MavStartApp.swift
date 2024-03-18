0abbbe3ad112243b9d3745f485d2f6e96ea6e6a4
dominikcsiszer <dominikcsiszer@icloud.com>
2024-03-18 12:00:17 +0100

    Initial Commit

---
+++ MavStart/MavStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MavStartApp.swift
+//  MavStart
+//
+//  Created by Csiszér Dominik on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MavStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
